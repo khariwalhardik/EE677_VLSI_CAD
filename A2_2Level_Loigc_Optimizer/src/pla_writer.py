6bc36d46483dc554acef028dce45a92edcdb0cb4
KHARIWAL <128465167+khariwalhardik@users.noreply.github.com>
2025-09-06 18:26:19 +0530

    chore: remove unnecessary comments

--- A2_2Level_Loigc_Optimizer/src/pla_writer.py
+++ A2_2Level_Loigc_Optimizer/src/pla_writer.py
@@ -1,8 +1,8 @@
-from cube import Cube
+from .cube import Cube
 
 def write_pla(filename: str, input_labels, output_labels, selected_cubes_per_output):
     """
-    Write optimized cubes to a PLA file.
+    Write optimized cubes to a PLA file, merging cubes across multiple outputs.
 
     Args:
         filename: path to output PLA file
@@ -13,24 +13,25 @@ def write_pla(filename: str, input_labels, output_labels, selected_cubes_per_out
     num_inputs = len(input_labels)
     num_outputs = len(output_labels)
 
+    # Merge cubes across outputs
+    cube_dict = {}  # key = input pattern, value = list of output bits
+    for out_idx, cubes in enumerate(selected_cubes_per_output):
+        for cube in cubes:
+            if cube.pattern not in cube_dict:
+                cube_dict[cube.pattern] = ['0'] * num_outputs
+            cube_dict[cube.pattern][out_idx] = '1'
+
     with open(filename, "w") as f:
-        # Write PLA headers
+        # PLA headers
         f.write(f".i {num_inputs}\n")
         f.write(f".o {num_outputs}\n")
         f.write(".ilb " + " ".join(input_labels) + "\n")
         f.write(".ob " + " ".join(output_labels) + "\n")
 
         # Write cubes
-        # For each output, create a line for each cube
-        for out_idx, cubes in enumerate(selected_cubes_per_output):
-            for cube in cubes:
-                # Generate output pattern string
-                output_pattern = ['0'] * num_outputs
-                output_pattern[out_idx] = '1'
-                line = cube.pattern + " " + "".join(output_pattern) + "\n"
-                f.write(line)
+        for in_pattern, out_bits in cube_dict.items():
+            f.write(f"{in_pattern} {''.join(out_bits)}\n")
 
-        # End of PLA
         f.write(".e\n")
 
 
@@ -38,15 +39,14 @@ def write_pla(filename: str, input_labels, output_labels, selected_cubes_per_out
 # Quick Test
 # --------------------------
 if __name__ == "__main__":
-    from cube import Cube
-
-    # Example headers
     inputs = ['a', 'b', 'c']
-    outputs = ['f']
+    outputs = ['f0', 'f1']
 
-    # Example selected prime cubes
-    selected_cubes = [{Cube("11-"), Cube("1-1")}]
+    # Example selected cubes per output
+    selected_cubes = [
+        {Cube("11-"), Cube("1-1")},  # Output f0
+        {Cube("11-"), Cube("01-")}   # Output f1
+    ]
 
-    # Write to output PLA
-    write_pla("outputs/example1_optimized.pla", inputs, outputs, selected_cubes)
-    print("Optimized PLA written to outputs/example1_optimized.pla")
+    write_pla("outputs/example_merged.pla", inputs, outputs, selected_cubes)
+    print("Optimized PLA written to outputs/example_merged.pla")
